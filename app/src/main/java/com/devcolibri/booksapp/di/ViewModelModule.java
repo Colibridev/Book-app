bab8e568554292984068808f6fd6674f0cb6ded4
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-01-23 16:35:09 +0300

    [ADD] missed classes ViewModelFactory, ViewModelModule

---
+++ app/src/main/java/com/devcolibri/booksapp/di/ViewModelModule.java
@@ -0,0 +1,24 @@
+package com.devcolibri.booksapp.di;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.devcolibri.booksapp.BookDetailsViewModel;
+import com.devcolibri.booksapp.BookListViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class ViewModelModule {
+
+    @IntoMap
+    @Binds
+    @ViewModelKey(BookListViewModel.class)
+    public abstract ViewModel providesBookListViewModel(BookListViewModel bookListViewModel);
+
+    @IntoMap
+    @Binds
+    @ViewModelKey(BookDetailsViewModel.class)
+    public abstract ViewModel providesBookDetailsViewModel(BookDetailsViewModel bookDetailsViewModel);
+}
