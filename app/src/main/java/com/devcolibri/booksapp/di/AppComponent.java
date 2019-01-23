ecda80aad0af223215d1ae622b636cabad98ff14
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-01-23 16:11:53 +0300

    Add dagger architecture components support

--- app/src/main/java/com/devcolibri/booksapp/di/AppComponent.java
+++ app/src/main/java/com/devcolibri/booksapp/di/AppComponent.java
@@ -2,11 +2,15 @@ package com.devcolibri.booksapp.di;
 
 import javax.inject.Singleton;
 
+import com.devcolibri.booksapp.DetailsActivity;
+import com.devcolibri.booksapp.MainActivity;
+
 import dagger.Component;
 
-@Component(modules = AppModule.class)
+@Component(modules = {AppModule.class, ViewModelModule.class})
 @Singleton
 public interface AppComponent {
-    BookListComponent plusBookListComponent();
-    BookDetailsComponent plusBookDetailsComponent();
+
+    void inject(MainActivity mainActivity);
+    void inject(DetailsActivity detailsActivity);
 }
